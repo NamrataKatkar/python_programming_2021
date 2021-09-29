e9f419ca5e183b917cbcc49695cc60f26c8e8fe3
NamrataKatkar <51794565+NamrataKatkar@users.noreply.github.com>
2021-09-28 23:38:20 -0400

    Update BinaryStringEnumeration.py

--- BinaryStringEnumeration.py
+++ BinaryStringEnumeration.py
@@ -1,6 +1,7 @@
 #Enumerate all possible binary strings of length n . (meaning, create all possible combinations of 0 and 1 of length n). ck
 
-#using recursion
+#approach 1
+#using recursion - More job to do in logic section
 def binaryString(n):
     if n==1:
         return ['0','1']
@@ -11,8 +12,22 @@ def binaryString(n):
             res.append(s+'0')
             res.append(s+'1')
         return res
-        
+    
+#approach 2
+#using recursion - Less job to do in logic section   
+def binaryString(n):
+    binaryStringhelper("",n)
+    
+def binaryStringhelper(slate,n):
+    if n==0:
+        print (slate)
+    else:
+        binaryStringhelper(slate+'0',n-1)
+        binaryStringhelper(slate+'1',n-1)
+         
+
+#main function
 if __name__=='__main__':
-    print(binaryString(3))
+    binaryString(3)
     
  
