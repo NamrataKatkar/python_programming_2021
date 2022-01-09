82e3ae4b1c990dd7a409e21274315bfa99eeab27
NamrataKatkar <51794565+NamrataKatkar@users.noreply.github.com>
2022-01-08 21:48:11 -0500

    Create CountSort.py

---
+++ CountSort.py
@@ -0,0 +1,18 @@
+# sort the array with k numbers in it (like student age)
+def countSort(a,maxnumber):
+    d=[0]*maxnumber
+    for i in range(0,len(a)):
+        d[a[i]]=d[a[i]]+1
+    res=[]
+    for j in range(0,len(d)):
+        if j==0:
+            j+=1
+        else:
+            for k in range(0,d[j]):
+                res.append(j)
+    return res
+
+
+if __name__=='__main__':
+    a=[9,8,7,2,2,1,0,9]
+    print(countSort(a,10))
